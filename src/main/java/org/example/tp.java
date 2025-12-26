14ec983565c73339ae9ad81baa1e98505117d3e4
Bousselham el-abd <bousselham.elabd2000@gmail.com>
2025-12-26 16:57:55 +0100

    first ftgcommit

--- src/main/java/org/example/tp.java
+++ src/main/java/org/example/tp.java
@@ -1,16 +1,20 @@
 package org.example;
 
 public class tp {
-    public static void main(String[] args) {
 
-        int[] notes = {12, 15, 10, 18, 14};
+    // Méthode pour calculer la somme des notes
+    public static int calculerSomme(int[] notes) {
         int somme = 0;
-
         for (int note : notes) {
             somme += note;
         }
-
-        System.out.println("Somme des notes = " + somme);
+        return somme;
+        //jrjktgrjegit
     }
 
+    public static void main(String[] args) {
+        int[] notes = {12, 15, 10, 18, 14};
+        int somme = calculerSomme(notes);
+        System.out.println("Somme des notes = " + somme);
+    }
 }
--- src/test/java/org/example/tpTest.java
+++ src/test/java/org/example/tpTest.java
@@ -1,7 +1,17 @@
 package org.example;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class tpTest {
 
+    @Test
+    void testCalculerSomme() {
+        int[] notes = {12, 15, 10, 18, 14};
+        int resultatAttendu = 69;
+
+        int resultat = tp.calculerSomme(notes);
+
+        assertEquals(resultatAttendu, resultat, "La somme des notes doit être correcte");
+    }
 }
